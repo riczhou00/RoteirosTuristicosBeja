aa64a3f57e81e1f1f0cdb9c4874344da8e74a857
Ricardo Zhou <21130@stu.ipbeja.pt>
2021-06-02 10:59:47 +0100

    Projeto_Android

---
+++ RoteiroTuristicoBeja_02/app/src/main/java/com/example/roteiroturisticobeja_02/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.roteiroturisticobeja_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.AbsListView;
+import android.widget.Adapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[];
+    int images[] = {R.drawable.castelo_de_beja, R.drawable.museu_regional, R.drawable.nucleo, R.drawable.museu_visigotico, R.drawable.igreja, R.drawable.jardim};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView= findViewById(R.id.recycleView);
+
+        s1 = getResources().getStringArray(R.array.Bem_vindo_a_Beja);
+        MyAdapter myAdapter = new MyAdapter(this, s1, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
